93994af039213e2e7d60c4fa3782220fb42d058f
OfirAndroid <dummyforwork3@gmail.com>
2019-11-01 10:44:22 +0200

    added retrofit lifecycle glide and network librarys and cart fragment

---
+++ app/src/main/java/com/example/cheetahcart/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cheetahcart
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/example/cheetahcart/cart/CartViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.cheetahcart.cart
+
+import androidx.lifecycle.ViewModel
+
+class CartViewModel : ViewModel() {
+
+    private val TAG = CartViewModel::class.java.simpleName
+
+
+}
